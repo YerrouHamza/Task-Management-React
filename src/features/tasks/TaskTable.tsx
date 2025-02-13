5a069e8827c826e822537496e06e0821ea4b1b92
yerrouhamza <hamzayerrou5@yahoo.com>
2025-02-14 00:47:28 +0100

    craeted the add new task feature

--- src/features/tasks/TaskTable.tsx
+++ src/features/tasks/TaskTable.tsx
@@ -1,12 +1,27 @@
-import { useTasks } from "./store/useTasksStore";
+import { useState } from "react";
+
 import { Table, TableBody, TableHead, TableHeader, TableRow } from "@/components/ui/table";
+import { Button } from "@/components/ui/button";
+
+import { useTasks } from "./store/useTasksStore";
 import { TaskRow } from "./components/TaskRow";
+import NewTaskInput from "./components/NewTaskInput";
 
 export function TaskTable() {
   const { tasks } = useTasks();
+  const [newTask, setNewTask] = useState(false)
 
   return (
-    <div>
+    <div className="space-y-3">
+      <div className="flex justify-end items-center">
+        <Button 
+          size='sm'
+          variant={!newTask ? 'default' : 'outline'}
+          onClick={() => setNewTask(!newTask)}
+        >
+          {!newTask ? <span>New Task</span> : <span>Close Task</span>}
+        </Button>
+      </div>
       <Table className="w-full bg-gray-50 border brder-gray-200 rounded-lg">
         <TableHeader>
           <TableRow>
@@ -18,6 +33,8 @@ export function TaskTable() {
           {tasks.map((task) => (
             <TaskRow key={task.id} task={task} />
           ))}
+
+          {newTask ? <NewTaskInput closeNewTaskTab={setNewTask} /> : null}
         </TableBody>
       </Table>
     </div>
---
+++ src/features/tasks/components/newTaskInput.tsx
@@ -0,0 +1,44 @@
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { TableCell, TableRow } from "@/components/ui/table";
+import { useState } from "react";
+import { useTasks } from "../store/useTasksStore";
+
+export default function NewTaskInput({closeNewTaskTab}: {
+  closeNewTaskTab: (value: boolean) => void
+}) {
+    const { addTask } = useTasks()
+    const [newTask, setNewTask] = useState("");
+
+
+    const handleAddTask = () => {
+      if(newTask.trim() !== "") {
+        addTask({todo: newTask, status: 'pending'})
+        setNewTask("");
+        closeNewTaskTab(false)
+      }
+    }
+    
+    return (
+      <TableRow className="bg-white">
+        <TableCell colSpan={3}>
+          <Input
+            value={newTask}
+            onChange={(e) => setNewTask(e.target.value)}
+            onKeyDown={(e) => {
+              if (e.key === "Enter") {
+                handleAddTask()
+              }
+            }}
+            placeholder="Add new task..."
+          />
+        </TableCell>
+        <TableCell>
+          <Button
+            className="w-full"
+            onClick={() => handleAddTask()}
+          >Add</Button>
+        </TableCell>
+      </TableRow>
+    );
+  }
